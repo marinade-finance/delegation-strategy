a64c894b88db9830029620585d96ed734904d405
janlegner <32453746+janlegner@users.noreply.github.com>
2022-10-22 08:33:31 +0200

    Gauges control 15 % of TVL (#7)

--- cli/score-post-process/src/process_scores.rs
+++ cli/score-post-process/src/process_scores.rs
@@ -86,7 +86,7 @@ pub struct ProcessScoresOptions {
     #[structopt(
         long = "vote-gauges-stake-pct",
         help = "How much of total stake is affected by votes.",
-        default_value = "10" // %
+        default_value = "15" // %
     )]
     pub vote_gauges_stake_pct: u32,
 
